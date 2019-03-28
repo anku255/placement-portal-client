dc1817f3263a38544573e7131b7c37576b5fbff8
Ankit Tiwari <ankitt255@gmail.com>
2019-03-28 21:13:17 +0530

    ⚙ Change class to className

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <div className="container landing">
-      <section class="hero is-large has-text-centered">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="title is-size-1">
+      <section className="hero is-large has-text-centered">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title is-size-1">
               Welcome to IIIT Kalyani Placement Portal
             </h1>
-            <h2 class="subtitle is-size-3">
+            <h2 className="subtitle is-size-3">
               <Link to="/cv">Click Here</Link> to manage your CV
             </h2>
           </div>
